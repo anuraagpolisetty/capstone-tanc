94a1d1c3748c0bd15be81f425f17dd9bf539f223
anuraagpolisetty <anuraag@uw.edu>
2020-03-03 23:28:11 -0800

    Created impact measure index functions

---
+++ app/IndexData.R
@@ -0,0 +1,92 @@
+library(dplyr)
+library(plotly)
+
+
+allData <- read.csv(file="./data/TOTAL.csv", stringsAsFactors=FALSE)
+
+# Convert df to index scale (1-3)
+IndexData <- function(df) {
+  questions <- colnames(df)[12:25]
+  for (q in questions) {
+    df[is.na(df[q]), q] <- '0'
+    df[df[q] == 'Not Applicable',q] <- '0'
+    df[df[q] == 'Almost Never',q] <- '1'
+    df[df[q] == 'Sometimes',q] <- '2'
+    df[df[q] == 'Most of the Time',q] <- '3'
+    df[q] <- as.numeric(df[[q]])
+  }
+  return (df)
+}
+
+
+# create indexes for every target impact measure
+
+SocialIndex <- function(df, site){
+  if(site == "all"){
+    # social.questions <- c(12, 13)
+    social.life <- df[,c(12, 13)] #social.questions]
+  }
+  index <- mean(colMeans(social.life))
+  return (index)
+}
+# social.index <- SocialIndex(data.2019, "all")
+
+
+PhysicalIndex <- function(df, site) {
+  if(site == "all"){
+    # physical.questions <- c(14, 15, 16, 24)
+    physical.health <- df[,c(14, 15, 16, 24)] #physical.questions]
+  }
+  index <- mean(colMeans(physical.health), na.rm=TRUE)
+  return (index)
+}
+# physical.index <- PhysicalIndex(data.2019, "all")
+
+
+PositiveIndex <- function(df, site) {
+  if(site == "all") {
+    # positive.questions <- c(17, 18, 22)
+    positive.outlook <- df[, c(17, 18, 22)] #positive.questions]
+  }
+  index <- mean(colMeans(positive.outlook, na.rm=TRUE))
+  return (index)
+}
+# positive.index <- PositiveIndex(data.2019, "all")
+
+
+
+ServicesIndex <- function(df, site) {
+  if(site == "all") {
+    # services.questions <- c(19, 23)
+    access.to.services <- df[,c(19, 23)] #services.questions]
+  }
+  index <- mean(colMeans(access.to.services, na.rm=TRUE))
+  return (index)
+}
+# services.index <- ServicesIndex(data.2019, "all")
+
+
+IndependenceIndex <- function(df, site) {
+  if(site == "all"){
+    # independence.questions <- c(20)
+    independence <- df[, 20] # independence.questions]
+  }
+  index <- mean(independence, na.rm = TRUE)
+  return (index)
+}
+# independence.index <- IndependenceIndex(data.2019, "all")
+
+
+OverallSatisfactionIndex <- function(df, site) {
+  if(site == "all") {
+    # satisfaction.questions <- c(21,25)
+    overall.satisfaction <- df[,c(21,25)] # satisfaction.questions]
+  }
+  index <- mean(colMeans(overall.satisfaction, na.rm=TRUE))
+  return (index)
+}
+# satisfaction.index <- OverallSatisfactionIndex(data.2019, "all")
+
+
+
+
---
+++ app/scripts/GaugeChart.R
@@ -0,0 +1,51 @@
+library(plotly)
+source("./IndexData.R")
+
+allData <- read.csv(file="./data/TOTAL.csv", stringsAsFactors=FALSE)
+
+data.2019 <- IndexData(allData) %>% filter(Batch=="2019-1"|Batch=="2019-2")
+social.index.2019 <- SocialIndex(data.2019, "all")
+
+data.2018 <- IndexData(allData) %>% filter(Batch=="2018-1"|Batch=="2018-2")
+social.index.2018 <- SocialIndex(data.2018, "all")
+
+social.index <- SocialIndex(data.2019, "all")
+physical.index <- PhysicalIndex(data.2019, "all")
+positive.index <- PositiveIndex(data.2019, "all")
+services.index <- ServicesIndex(data.2019, "all")
+independence.index <- IndependenceIndex(data.2019, "all")
+satisfaction.index <- OverallSatisfactionIndex(data.2019, "all")
+
+
+GaugeChart <- function(data, FUN, year) {
+  yr1 <- paste0(year,"-1")
+  yr2 <- paste0(year, "-2")
+  index <- FUN(data)
+  
+  print(FUN(data, "all"))
+  fig <- plot_ly(
+    domain = list(x = c(0, 1), y = c(0, 1)),
+    value = social.index.2019,
+    title = list(text = "Social Index"),
+    type = "indicator",
+    mode = "gauge+number+delta",
+    delta = list(reference = social.index.2018),
+    gauge = list(
+      axis =list(range = list(NULL, 3)),
+      bar = list(color="lightgreen")
+      # steps = list(
+      #   list(range = c(0, 250), color = "red"),
+      #   list(range = c(250, 400), color = "yellow"),
+      #   list(range = c(400, 500), color = "green"))
+      # threshold = list(
+      #   line = list(color = "red", width = 4),
+      #   thickness = 0.75,
+      #   value = 490)
+    ))
+  fig <- fig %>%
+    layout(margin = list(l=20,r=30))
+  return(fig)
+}
+
+GaugeChart(data.2018, SocialIndex)
+
