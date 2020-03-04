4d0716cc679e94f5461d58950578ef263a045467
Tabitha <andert97@uw.edu>
2020-03-03 17:17:12 -0800

    beginning stages of Shiny app

--- app/ui.R
+++ app/ui.R
@@ -1,40 +1,109 @@
 library(shiny)
 library(ggplot2)
 library(bs4Dash)
+library(plotly)
 
 fluidPage(
-  # navbarPage("Survey Dashboard Tool",
-  #   tabPanel("Home"),
-  #   tabPanel("Create"),
-  #   tabPanel("Data"),
-  #   tabPanel("Upload")),
-  ui = bs4DashPage(
-    navbar = bs4DashNavbar(),
+  bs4DashPage(
+    title = 'Survey Visualization Tool',
+    sidebar_collapsed = FALSE,
+    
+    # Navbar on top
+    navbar = bs4DashNavbar(
+      skin = 'dark',
+      status = 'primary'
+    ),
+    
+    # left sidebar
+    sidebar = bs4DashSidebar(
+      skin = "dark",
+      status = 'primary',
+      title = 'Survey Visualization Tool',
+      
+      bs4SidebarMenu(
+        bs4SidebarMenuItem(
+          'Home',
+          tabName = 'home',
+          icon = 'info'
+        ),
+        bs4SidebarMenuItem(
+          'Tabitha/Anuraag Visuals',
+          tabName = 'create',
+          icon = 'info'
+        ), 
+        bs4SidebarMenuItem(
+          'Data',
+          tabName = 'data',
+          icon = 'info'
+        )
+      )
+    ),
+    
+    
     bs4DashBody(
       shinyjs::useShinyjs(),
       shinyWidgets::chooseSliderSkin("HTML5"),
       bs4TabItems(
         bs4TabItem(
-          tabName = "welcome",
-          bs4Jumbotron(
-            title = "Welcome to the LEGO Mosaic Maker!",
-            lead = "This is a Shiny application that lets you convert any picture to a LEGO mosaic directly from the comfort of your web browser!  Once you upload a picture, you can customize many settings.  This app would not be possible without the innovative R scripts created by Ryan Timpe!  Here are links to his excellent blog posts detailing the workflow:",
-            list_to_li(
-              list(
-                tags$a(href = "http://www.ryantimpe.com/post/lego-mosaic1/", "How To: LEGO mosaics from photos using R & the tidyverse"),
-                tags$a(href = "http://www.ryantimpe.com/post/lego-mosaic2/", "LEGO mosaics: Two weeks later"),
-                tags$a(href = "http://www.ryantimpe.com/post/lego-mosaic3/", "LEGO mosaics: Part 3(D)")
-              )
+          tabName = "home",
+          bs4UserCard(
+            title = "Pike Market Senior Center",
+            width = 12,
+            bs4InfoBox(
+              title = 'View Data',
+              tabName = 'data',
+              gradientColor = 'primary'
+            )
             ),
-            status = "primary",
-            btn_name = "App GitHub Repository",
-            href = "https://gitlab.com/rpodcast/shinylego"
+          bs4UserCard(
+            title = "Wallingford Senior Center",
+            width = 12,
+            status = 'primary',
+            bs4InfoBox(
+                title = 'View Data',
+                tabName = 'View Data',
+                gradientColor = 'primary'
+            )
+          ),
+          bs4UserCard(
+            title = "Greenwood Senior Center",
+            width = 12,
+            bs4InfoBox(
+              title = 'View Data',
+              tabName = 'View Data',
+              gradientColor = 'primary'
+            )
+          ),
+          bs4UserCard(
+            title = "South Park Senior Center",
+            width = 12,
+            bs4InfoBox(
+              title = 'View Data',
+              tabName = 'View Data',
+              gradientColor = 'primary'
+            )
+          )
+          ),
+          bs4TabItem(
+            tabName = 'create',
+            selectInput("select", label=h3('Select box'),
+                        choices = list('Question 1' = 1, 'Question 2' = 2),
+                        selected = 1),
+            bs4Card(
+              title = "Over Time",
+              width = 12,
+              collapsible = TRUE,
+              plotOutput("timeplot")
+            )
+          ),
+          bs4TabItem(
+            tabName = 'data'
           )
         )
       )
     )
   )
-)
+
     # Use this styling to move on tab over. How to move the rest?
     # tags$head(tags$style('.navbar-nav {width: 95%;}
     #                       .navbar-nav :first-child{float:right}')))
