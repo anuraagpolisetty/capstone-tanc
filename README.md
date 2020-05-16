87c075137539537939f48fc005e388f13c1f15ba
anuraagpolisetty <anuraag@uw.edu>
2020-05-15 19:32:29 -0700

    Updated readme and changed survey

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
-# capstone-tanc
-This is Team TANC's capstone github repo for our project's code, documentation, and related artifacts
+# ADS Dashboard
+This is Team TANC's capstone github repo for our project's code, documentation, and related artifacts. We are building a dashboard for the City of Seattle's Aging and Disability Services to use for survey responses, impact measures, and data analysis. 
 
 # Contact Information
 Tabitha Anderson: andert97@uw.edu
 
 Anuraag Polisetty: anuraag@uw.edu
 
+Naved Krishnamurthy: navedk@uw.edu
+
 Christopher Vitalis: cvitalis@uw.edu
 
-Naved Krishnamurthy: navedk@uw.edu
--- app/constants.R
+++ app/constants.R
@@ -42,5 +42,4 @@ income_brackets <- c("Very Low: Less than $18550 for 1 person; or $21,2000 for 2
 
 # Do more volunteer work	See friends more often/make new friends	Take better care of my health	Eat meals that are better for me	Have more energy	Feel happier or more satisfied with my life	Have something to look forward to each day	Know where to ask if I need a service such as a ride to a doctor or an aide	Feel more able to stay independent	Feel that the senior center has had a positive effect on my life	Learn new things	Have learned about services and benefits	Am more physically active	Would recommend the senior center to a friend or family member	Please tell us how participating in the senior center has changed your life	I participate in the following activities at the senior center	Race - American Indian or Alaska Native	Race - Asian, Asian-American	Race - Black, African-American, Other African	Race - Hawaiian Native or Pacific Islander	Race - Hispanic, Latino	Race - Other	Race - White or Caucasian	Race	What is your zip code	Other (please specify)	What is your estimated annual income	Extra
 
-# change survey ui h5 question size
 # 
